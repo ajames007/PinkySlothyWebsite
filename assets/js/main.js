81d7653e9251c061bd64e30f18cd02165dd4e422
ajames007 <chanakarn.sus@ku.th>
2023-03-27 00:31:03 +0700

    loginBtn bug fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile().then((Response)=>{
-    console.log(Response);
-  }) ;
-
-  console.log("ได้แล้ว")
-}
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      alert("You've been signOut sucessfully");
-      $(".g-signin2").css("display","block");
-      $(".data").css("display","none");
+function onSignIn() {
+  gapi.load('auth2', function() {
+    gapi.auth2.init({
+      client_id: '85833623378-nvjnrb68dtph5qka8s27m3ob2kumnlas.apps.googleusercontent.com',
+    }).then(function(auth2) {
+      auth2.signIn().then(function(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('Name: ' + profile.getName());
+        console.log('Email: ' + profile.getEmail());
+        console.log('Image URL: ' + profile.getImageUrl());
+        // TODO: Do something with the user data, like send it to a server or update the UI.
+      }, function(error) {
+        console.log('Sign-in error', error);
+      });
     });
-  }
+  });
+}
