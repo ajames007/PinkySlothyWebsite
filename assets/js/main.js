766eb953315ef342a438cb4e33242e4348266a92
ajames007 <chanakarn.sus@ku.th>
2023-03-27 19:11:14 +0700

    loginBtn bugfixing 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
-function onSignIn() {
-  gapi.load('auth2', function() {
-    gapi.auth2.init({
-      client_id: '85833623378-nvjnrb68dtph5qka8s27m3ob2kumnlas.apps.googleusercontent.com',
-    }).then(function(auth2) {
-      auth2.signIn().then(function(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('Name: ' + profile.getName());
-        console.log('Email: ' + profile.getEmail());
-        console.log('Image URL: ' + profile.getImageUrl());
-        // TODO: Do something with the user data, like send it to a server or update the UI.
-      }, function(error) {
-        console.log('Sign-in error', error);
-      });
-    });
-  });
+// Get the sign-in button and the start button elements
+const signInBtn = document.getElementById("g_id_signin");
+const startBtn = document.getElementById("start-button");
+
+// Check if the user is logged in
+if (GoogleAuth.isSignedIn.get()) {
+  // Hide the sign-in button and show the start button
+  signInBtn.style.display = "none";
+  startBtn.style.display = "block";
+} else {
+  // Show the sign-in button and hide the start button
+  signInBtn.style.display = "block";
+  startBtn.style.display = "none";
 }
