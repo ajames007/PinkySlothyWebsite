2f1ea2b7c21a911661a891b2b0f1e6bf64a4f1d4
ajames007 <chanakarn.sus@ku.th>
2023-03-27 20:23:45 +0700

    add google singIn API in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,43 @@
 // Get the sign-in button and the start button elements
 const signInBtn = document.getElementById("g_id_signin");
 const startBtn = document.getElementById("start-button");
-GoogleAuth = gapi.auth2.getAuthInstance();
+// Load the Google Sign-In API
+gapi.load('auth2', function() {
+  // Initialize the Google Sign-In API
+  gapi.auth2.init({
+    client_id: 'YOUR_CLIENT_ID',
+    // Other optional parameters
+  }).then(function() {
+    // Obtain a reference to the GoogleAuth object
+    GoogleAuth = gapi.auth2.getAuthInstance();
+    
+    // Check if the user is signed in
+    if (GoogleAuth.isSignedIn.get()) {
+      // The user is signed in, do something here
+      var user = GoogleAuth.currentUser.get();
+      var profile = user.getBasicProfile();
+      console.log('User email:', profile.getEmail());
+    } else {
+      // The user is not signed in, do something else here
+      console.log('User is not signed in');
+    }
+    
+    // Sign the user in programmatically
+    GoogleAuth.signIn().then(function() {
+      console.log('User signed in successfully');
+    }, function(error) {
+      console.error('Error signing in:', error);
+    });
+    
+    // Sign the user out programmatically
+    GoogleAuth.signOut().then(function() {
+      console.log('User signed out successfully');
+    }, function(error) {
+      console.error('Error signing out:', error);
+    });
+  });
+});
+
 
 // Check if the user is logged in
 if (GoogleAuth.isSignedIn.get()) {
